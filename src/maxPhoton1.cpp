c8c18684c0c8c350764799c6113b2343ae19022a
ravenOSS <35054853+ravenOSS@users.noreply.github.com>
2019-05-18 22:07:11 -0600

    Change local vars to global Remove Serial1.flush Change serial available logical test

--- src/maxPhoton1.cpp
+++ src/maxPhoton1.cpp
@@ -8,8 +8,6 @@
  */
 
 void setup();
-void loop();
-int MAXread();
 #line 8 "/Users/raventt/MBP_Projects/arduino/maxPhoton1/src/maxPhoton1.ino"
 void setup()
 {
@@ -19,31 +17,54 @@ void setup()
   Serial.println("Sonar connected");
 }
 
-void loop()
-{
-  int range = MAXread();
-  Serial.print("Range: ");
-  Serial.println(range);
-  delay(1200); //set interval for sensor reading
-}
+// void loop()
+// {
+//   int range = readMax();
+//   Serial.print("Range: ");
+//   Serial.println(range);
+//   delay(1200); //set interval for sensor reading
+// }
 
-int MAXread()
-{
-  uint8_t length = 4;
-  char buffer[5];
+// int readMax()
+// {
+//   uint8_t length = 4;
+//   char inBuf[length];
+//   char inByte;
 
-  // flush and wait for a range reading
-  Serial1.flush();
+//   // flush and wait for a range reading
+//   Serial1.flush();
+ 
+//   while (Serial1.available() > 0  && Serial1.read() != 'R') {
+//     inByte = Serial1.read();
+//     if (inByte == 'R') {
+//       for (int i = 0; i < 3; i++) {
+//         inBuf[i] = Serial1.read();
+//       }
+//     }
+//     return atoi(inBuf);
+//   }
+//   return 0;
+// }
 
-  while (!Serial1.available() || Serial1.read() != 'R');
+// int readMax()   // following code is simply illogical && does not work
+//                 // code extracted from another library
+//                 // may not work because maxbotix.h (from original library) is deliberately not included
+// {
+//   uint8_t length = 4;
+//   char buffer[5];
 
-  // read the range
-  for (int i = 0; i < length; i++)
-  {
-    while (!Serial1.available());
+//   // flush and wait for a range reading
+//   Serial1.flush();
+
+//   while (Serial1.available() > 0 || Serial1.read() != 'R');
+
+//   // read the range
+//   for (int i = 0; i < length; i++)
+//   {
+//     while (!Serial1.available());
   
-    buffer[i] = Serial1.read();
-  }
+//     buffer[i] = Serial1.read();
+//   }
 
-  return atoi(buffer);
-}
+//   return atoi(buffer);
+// }
--- src/maxPhoton1.ino
+++ src/maxPhoton1.ino
@@ -5,39 +5,42 @@
  * Date: 5.13.19
  */
 
+// global vars (why?)
+
+ const int length = 4;
+ char inBuf[length];
+ char inChar;
+
 void setup()
 {
   Serial.begin(9600);
   Serial.println("Hello ravenTT!");
-  Serial1.begin(9600);
+  delay(250);
+  Serial1.begin(9600, SERIAL_8N1);
   Serial.println("Sonar connected");
 }
 
 void loop()
 {
-  int range = MAXread();
+  int range = readMax();
   Serial.print("Range: ");
   Serial.println(range);
   delay(1200); //set interval for sensor reading
 }
 
-int MAXread()
+int readMax()
 {
-  uint8_t length = 4;
-  char buffer[5];
-
   // flush and wait for a range reading
-  Serial1.flush();
-
-  while (!Serial1.available() || Serial1.read() != 'R');
-
-  // read the range
-  for (int i = 0; i < length; i++)
-  {
-    while (!Serial1.available());
-  
-    buffer[i] = Serial1.read();
+  // Serial1.flush();
+ 
+  while (Serial1.available() > 0  && Serial1.read() != 'R') {
+    inChar = Serial1.read();
+    if (inChar == 'R') {
+      for (int i = 0; i < 4; i++) {
+        inBuf[i] = Serial1.read();
+      }
+    }
+    return atoi(inBuf);
   }
-
-  return atoi(buffer);
+  return 0;
 }
